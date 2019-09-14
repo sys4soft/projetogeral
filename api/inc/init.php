a194cce4bf873fd1cf17e989324e89742beb4a89
sys4soft <sys4soft@gmail.com>
2019-09-14 16:58:04 +0100

    JR

--- api/inc/init.php
+++ api/inc/init.php
@@ -17,7 +17,27 @@
     /* if there is no post information, data will be always an empty array */
     if(!is_array($data)){
         $data = array();
+    }    
+
+    // ---------------------------------------------
+    // check if app_key exists
+    if(!key_exists('app_key', $data)){
+        $response['status'] = 'Missing app_key.';
+        echo json_encode($response);
+        die();
+    } else {
+        $params = array(
+            ':app_key' => $data['app_key']
+        );
+        $gestor = new cl_gestorBD();
+        $dTemp = $gestor->EXE_QUERY("SELECT id_app FROM stock_apps WHERE app_key = :app_key AND active = 1", $params);
+        if(count($dTemp) == 0){
+            $response['status'] = 'App without permission to access API.';
+            echo json_encode($response);
+            die();
+        }
     }
+    // ---------------------------------------------
 
     if(array_key_exists('Token',$data)){
         $Token = $data['Token'];
